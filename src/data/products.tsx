9d7d9ad50851f97be9b71a3007c3f1cf33847353
namdang0219 <namdang.contact@gmail.com>
2025-04-24 14:59:42 +0900

    fix eslint check when build

--- src/data/products.tsx
+++ src/data/products.tsx
@@ -1,8 +1,6 @@
 import { ProductType } from "@/types/ProductType";
 import { myMovies } from "./products/MyMovies.product";
 import { rekiteku } from "./products/Rekiteku.product";
-import { morimoriWebApp } from "./products/MorimoriWebApp.product";
-import { morimoriMobileApp } from "./products/MorimoriMobileApp.product";
 
 export const products: ProductType[] = [
 	myMovies,
